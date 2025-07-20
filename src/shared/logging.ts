fb0b8258011339ce92b9cbd03071950ae9219b3b
Pavel Erokhin <mairwunnx@gmail.com>
2025-07-20 19:36:23 +0300

    Добавлен класс ConsoleWrapper для улучшенного логирования в Telegram-боте MCBuddy, заменяющий стандартный логгер на консольный вывод с уровнями информации, предупреждений и ошибок.

--- src/shared/logging.ts
+++ src/shared/logging.ts
@@ -1,4 +1,22 @@
 import { Logger } from "tslog";
 
-export const logger = new Logger({ type: "json", name: "MCBuddy-Bot" });
+class ConsoleWrapper {
+  constructor(private readonly logger: Logger<any>) {
+    this.logger = logger;
+  }
+
+  info(message: string, ...args: any[]) {
+    console.log("[INFO] " + message, ...args);
+  }
+
+  warn(message: string, ...args: any[]) {
+    console.log("[WARN] " + message, ...args);
+  }
+
+  error(message: string, ...args: any[]) {
+    console.log("[ERROR] " + message, ...args);
+  }
+}
+
+export const logger = new ConsoleWrapper(new Logger({ type: "json", name: "MCBuddy-Bot" }));
 logger.info("🔄 Initialization of MCBuddy Bot");
